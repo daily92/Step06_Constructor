b467ec80264beeb3a5cf1a8b8404e0ae98c08db4
jiehee <daily92@hanmail.net>
2017-11-17 10:27:14 +0900

    MainClass04 수정 완료~~

--- Step06_Constructor/src/test/main/MainClass04.java
+++ Step06_Constructor/src/test/main/MainClass04.java
@@ -7,6 +7,24 @@ import test.house.Television;
 
 public class MainClass04 {
 	public static void main(String[] args) {
+		System.out.println("메인 메소드가 시작 되었습니다.");
+		//문자열 객체를 만드는 방법 두 가지
+		String a= "abcd";
+		String b= new String("abcd");
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println("--------");
+		
+		//문자열 내용은 같아도 참조값이 달라! 그래서 콘솔에 안 찍힘
+		if(a==b) {
+			System.out.println("a 와 b 는 참조값이 같아요~");
+		};
+		//문자열 내용 비교하려면? String 의 method 인 .equal 를 사용해야 해
+		if(a.equals(b)) {
+			System.out.println("a 와 b 는 내용이 같아요!");
+		}
+		
 		/*
 		 *  인자로 Aircon, MicroWave, Television 객체를 전달받는 생성자를 이용해서
 		 *  MyHouse 객체를 생성 후 
